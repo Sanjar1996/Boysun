903ddd7ee64f287c24dcbd5f00c2f87109343df4
Sanjar1996 <xabibovsanjarbek@gmail.com>
2021-11-19 17:26:03 +0500

    Update base.py

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -120,7 +120,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
